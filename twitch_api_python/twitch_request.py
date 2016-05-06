30416d9ac02ce406434f925d59219249f77d9958
Mark Penny <mark.penny.jm@gmail.com>
2016-05-05 21:24:21 -0400

    Basic list iteration in python and slicing

--- twitch_api_python/twitch_request.py
+++ twitch_api_python/twitch_request.py
@@ -1,1 +1,12 @@
-print('hello world');
+# A list is not an array
+games_on_twitch = ['League Of Legends','Dark Souls III', 'DOTA 2']
+
+for game in games_on_twitch:
+  # IndentationError if not properly indented
+  print(game)
+ 
+ 
+# slice inclusive_index:exclusive_index
+for game in games_on_twitch[0:1]:
+  # String concatenation with '+'
+  print('Most watched:' + game)
