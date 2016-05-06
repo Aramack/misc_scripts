6f517b492c7f888e2ccfbbe2cf0e36fb6299e749
Mark Penny <mark.penny.jm@gmail.com>
2016-05-05 21:54:29 -0400

    Tuples and conditionals

--- twitch_api_python/twitch_request.py
+++ twitch_api_python/twitch_request.py
@@ -1,12 +1,19 @@
 # A list is not an array
-games_on_twitch = ['League Of Legends','Dark Souls III', 'DOTA 2']
+games_on_twitch = [
+    # A tuple:
+    ('League Of Legends', 20000),
+    ('Dark Souls III', 15000),
+    ('DOTA 2', 10000)
+  ]
 
 for game in games_on_twitch:
   # IndentationError if not properly indented
-  print(game)
+  print(game[0] + ':' + str(game[1]))
  
  
 # slice inclusive_index:exclusive_index
-for game in games_on_twitch[0:1]:
-  # String concatenation with '+'
-  print('Most watched:' + game)
+for game in games_on_twitch[0:2]:
+  # If statement:
+  if game[1] > 18000:
+    # String concatenation with '+':
+    print('Most watched:' + str(game[0]))
