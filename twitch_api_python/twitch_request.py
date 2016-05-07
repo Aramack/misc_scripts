6448257bd6f8c535d6acc56f6aa971dc22c39c92
Mark Penny <mark.penny.jm@gmail.com>
2016-05-06 22:33:03 -0400

    Introduction to dictionaries and dictory iteration

--- twitch_api_python/twitch_request.py
+++ twitch_api_python/twitch_request.py
@@ -1,19 +1,26 @@
 # A list is not an array
-games_on_twitch = [
-    # A tuple:
+list_tuple = [
+    # A tuple, is an immutable list?
     ('League Of Legends', 20000),
     ('Dark Souls III', 15000),
     ('DOTA 2', 10000)
   ]
 
-for game in games_on_twitch:
+
+games_on_twitch = {
+    'League Of Legends': 20000,
+    'Dark Souls III': 15000,
+    'DOTA 2': 10000
+  }
+  
+for game in list_tuple:
   # IndentationError if not properly indented
   print(game[0] + ':' + str(game[1]))
  
  
 # slice inclusive_index:exclusive_index
-for game in games_on_twitch[0:2]:
+for game in games_on_twitch.keys():
   # If statement:
-  if game[1] > 18000:
+  if games_on_twitch[game] > 18000:
     # String concatenation with '+':
-    print('Most watched:' + str(game[0]))
+    print('Most watched ' + game + ':' + str(games_on_twitch[game]))
