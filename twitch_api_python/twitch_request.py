f40167d9f7fcb945b520a886ffb0d5ff0f47f5a1
Mark Penny <mark.penny.jm@gmail.com>
2016-05-07 22:45:47 -0400

    A basic function

--- twitch_api_python/twitch_request.py
+++ twitch_api_python/twitch_request.py
@@ -6,21 +6,23 @@ list_tuple = [
     ('DOTA 2', 10000)
   ]
 
-
-games_on_twitch = {
-    'League Of Legends': 20000,
-    'Dark Souls III': 15000,
-    'DOTA 2': 10000
-  }
-  
 for game in list_tuple:
   # IndentationError if not properly indented
   print(game[0] + ':' + str(game[1]))
- 
- 
-# slice inclusive_index:exclusive_index
-for game in games_on_twitch.keys():
-  # If statement:
-  if games_on_twitch[game] > 18000:
-    # String concatenation with '+':
-    print('Most watched ' + game + ':' + str(games_on_twitch[game]))
+  
+
+
+def get_games_top():
+  games_on_twitch = {
+      'League Of Legends': 20000,
+      'Dark Souls III': 15000,
+      'DOTA 2': 10000
+    }
+
+  for game in games_on_twitch.keys():
+    # If statement:
+    if games_on_twitch[game] > 18000:
+      # String concatenation with '+':
+      print('Most watched ' + game + ':' + str(games_on_twitch[game]))
+
+get_games_top()
