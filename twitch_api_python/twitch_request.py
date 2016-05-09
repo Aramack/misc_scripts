36c5c15185a028d6846574456376a1181d848a86
Mark Penny <mark.penny.jm@gmail.com>
2016-05-08 21:37:17 -0400

    Implementing classes and objects in python twitch API implementation

--- twitch_api_python/twitch_request.py
+++ twitch_api_python/twitch_request.py
@@ -1,28 +1,18 @@
-# A list is not an array
-list_tuple = [
-    # A tuple, is an immutable list?
-    ('League Of Legends', 20000),
-    ('Dark Souls III', 15000),
-    ('DOTA 2', 10000)
-  ]
+class Twitch_Request(object):
 
-for game in list_tuple:
-  # IndentationError if not properly indented
-  print(game[0] + ':' + str(game[1]))
-  
+  def __init__(self):
+    self.base_url = 'https://api.twitch.tv/kraken/'
+    self.http_request_header = 'Accept: application/vnd.twitchtv.v3+json'
+	
+  def get_games_top(self, limit=10, offset=0):
+    games_on_twitch = {
+        'League Of Legends': 20000,
+        'Dark Souls III': 15000,
+        'DOTA 2': 10000
+      }
+    for game in games_on_twitch.keys():
+      # If statement:
+      if games_on_twitch[game] > 18000:
+        # String concatenation with '+':
+        print('Most watched ' + game + ':' + str(games_on_twitch[game]))
 
-
-def get_games_top():
-  games_on_twitch = {
-      'League Of Legends': 20000,
-      'Dark Souls III': 15000,
-      'DOTA 2': 10000
-    }
-
-  for game in games_on_twitch.keys():
-    # If statement:
-    if games_on_twitch[game] > 18000:
-      # String concatenation with '+':
-      print('Most watched ' + game + ':' + str(games_on_twitch[game]))
-
-get_games_top()
---
+++ twitch_api_python/viewer_statsd.py
@@ -0,0 +1,4 @@
+from twitch_request import Twitch_Request
+
+my_request = Twitch_Request()
+my_request.get_games_top(my_request)
