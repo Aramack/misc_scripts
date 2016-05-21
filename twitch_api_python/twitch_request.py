075bb625449d8c86212d008e01ad150e7ec3d486
Mark Penny <mark.penny.jm@gmail.com>
2016-05-21 13:56:39 -0400

    Proper indentation and fixing too many argument exception

--- twitch_api_python/twitch_request.py
+++ twitch_api_python/twitch_request.py
@@ -7,23 +7,26 @@ class Twitch_Request(object):
     self.http_request_header = 'Accept: application/vnd.twitchtv.v3+json'
 
   def query(
-	  url, 
+      self,
+      url, 
       http_request_type = 'GET',
       request_params = ''
     ):
-	my_curl = pycurl.Curl()
-	my_curl.setopt(my_curl.URL, url)
-	if http_request_type == 'GET':
-	  url += '?' + request_params
-	elif http_request_type == 'POST':
-	  my_curl.setopt(my_curl.POSTFIELDS, request_params)
-	my_curl.perform()
-	my_curl.close()
+    my_curl = pycurl.Curl()
+    my_curl.setopt(my_curl.URL, url)
+    if http_request_type == 'GET':
+      url += '?' + request_params
+    elif http_request_type == 'POST':
+      my_curl.setopt(my_curl.POSTFIELDS, request_params)
+    my_curl.perform()
+    my_curl.close()
 	
 	
   def get_games_top(self, limit=10, offset=0):
+    pagination_params = 'limit=' + str(limit) + '&offset=' + str(offset)
+    api_endpoint = self.base_url + 'games/top'
     self.query(
-	    self.base_url + '/games/top',
+        api_endpoint,
         'GET',
-        'limit=' + limit + '&offset=' + offset
-	  )
+        pagination_params
+      )
