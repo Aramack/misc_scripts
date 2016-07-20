0d8ddcff74d5a7612ad425f0462ef962a03cdae3
Mark Penny <mark.penny.jm@gmail.com>
2016-07-19 22:40:08 -0400

    Add basic IO to the golang dice rolling program using string matching and regexp

--- golang/hello_world/roll_a_dice.go
+++ golang/hello_world/roll_a_dice.go
@@ -1,12 +1,38 @@
 package main
 
-import "fmt"
-import "time"
-import "math/rand"
+import (
+  "fmt"
+  "bufio"
+  "os"
+  "time"
+  "math/rand"
+  "regexp"
+  "strconv"
+  "strings"
+)
 
 func main() {
-  var sides_of_the_dice = 10
-  fmt.Println(get_number(sides_of_the_dice))
+  for {
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Print("What do you want? ")
+    input, _ := reader.ReadString('\n')
+	
+    r, _ := regexp.Compile("roll ([0-9]+)d([0-9]+)")
+    if (r.MatchString(input)) {
+	  d := r.FindStringSubmatch(input)
+	  fmt.Println("Rolling " + d[1] +"d" + d[2])
+	  number_of_dice, _ := strconv.Atoi(d[1])
+	  sides_of_the_dice, _ := strconv.Atoi(d[2])
+	  for i := 0; i < number_of_dice; i++ {
+        fmt.Println(get_number(sides_of_the_dice))
+	  }
+    } else if strings.Contains(input, "quit"){
+      fmt.Println("bye...")
+	  os.Exit(0)
+    } else {
+	  fmt.Println("I don't know how to do that")
+	}
+  }
 }
 
 /*
