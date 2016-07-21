af70312f135f02b00695468d0f246b63a9dafdb0
Mark Penny <mark.penny.jm@gmail.com>
2016-07-20 22:06:49 -0400

    Add a total to the rows

--- golang/hello_world/roll_a_dice.go
+++ golang/hello_world/roll_a_dice.go
@@ -9,38 +9,46 @@ import (
   "regexp"
   "strconv"
   "strings"
+  "bytes"
 )
 
 func main() {
+  roll_regex, _ := regexp.Compile("roll ([0-9]+)d([0-9]+)")
   for {
     reader := bufio.NewReader(os.Stdin)
     fmt.Print("What do you want? ")
     input, _ := reader.ReadString('\n')
-	
-    r, _ := regexp.Compile("roll ([0-9]+)d([0-9]+)")
-    if (r.MatchString(input)) {
-	  d := r.FindStringSubmatch(input)
-	  fmt.Println("Rolling " + d[1] +"d" + d[2])
-	  number_of_dice, _ := strconv.Atoi(d[1])
-	  sides_of_the_dice, _ := strconv.Atoi(d[2])
-	  for i := 0; i < number_of_dice; i++ {
-        fmt.Println(get_number(sides_of_the_dice))
-	  }
+
+    if (roll_regex.MatchString(input)) {
+      d := roll_regex.FindStringSubmatch(input)
+      fmt.Println("Rolling " + d[1] +"d" + d[2])
+      number_of_dice, _ := strconv.Atoi(d[1])
+      sides_of_the_dice, _ := strconv.Atoi(d[2])
+      var total = 0
+      for i := 0; i < number_of_dice; i++ {
+        roll := get_number(sides_of_the_dice)
+        total += roll
+		fmt.Println(roll)
+      }
+      var buffer bytes.Buffer
+      buffer.WriteString("Total: ")
+	  buffer.WriteString(strconv.Itoa(total))
+      fmt.Println(buffer.String())
     } else if strings.Contains(input, "quit"){
       fmt.Println("bye...")
-	  os.Exit(0)
+      os.Exit(0)
     } else {
-	  fmt.Println("I don't know how to do that")
-	}
+      fmt.Println("I don't know how to do that")
+    }
   }
 }
 
 /*
- Get a random number between [0, x)
+ Get a random number between [1, upper]
 */
 func get_number(upper int) int {
   seed := rand.NewSource(time.Now().UnixNano())
   random := rand.New(seed)
-  var number = random.Intn(upper)
+  var number = random.Intn(upper) + 1
   return number
 }
