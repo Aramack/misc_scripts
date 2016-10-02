c3754571ea97e8c81f276170be236d51e9a9e8b2
Mark Penny <mark.penny.jm@gmail.com>
2016-10-01 22:53:16 -0400

    working http.Client

--- golang/load_tester.go
+++ golang/load_tester.go
@@ -3,30 +3,24 @@ package main
 
 import (
   "fmt"
-  "log"
   "net/http"
-  "net/http/httputil"
-  "net/url"
-  "strings"
   "io/ioutil"
+  "time"
 )
 
-func http_get() {
-  req := &http.Request{
-          Method: "GET",
-          URL: &url.URL{
-            Host:   "https://example.com/",
-            Scheme: "https",
-          },
-    	  Header: http.Header{
-            "User-Agent": {"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36"},
-          },
-    }
-    out, err := httputil.DumpRequestOut(req, true)
-    if err != nil {
-      log.Fatal(err)
-    }
-    fmt.Println(strings.Replace(string(out), "\r", "", -1))
+func http_get(url string) {
+  request , err := http.NewRequest(
+    "GET",
+	url,
+	nil,
+  )
+  if (err != nil) {
+    return
+  }
+  client := &http.Client{
+    Timeout: time.Second * 30,
+  }
+  client.Do(request)
 }
 
 func http_get_2() {
@@ -37,5 +31,6 @@ func http_get_2() {
 }
 
 func main() {
-  http_get_2();
+  //http_get_2();
+  http_get("http://www.example.com")
 }
