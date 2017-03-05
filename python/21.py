df02c344dcfe737bc3d848822bf45a27fe7390db
Mark Penny <mark.penny.jm@gmail.com>
2017-03-04 23:48:35 -0500

    Remove enumerated types and replace with dictionary

--- python/21.py
+++ python/21.py
@@ -1,28 +1,30 @@
-from aenum import Enum
 import random
 
-Suit=['CLUB', 'DIAMOND', 'HEART', 'SPADE']
+Suit=['Clubs', 'Diamonds', 'Hearts', 'Spades']
   
-class Rank(Enum):
-  __order__ = 'ACE TWO THREE FOUR FIVE SIX SEVEN EIGHT NINE TEN JACK QUEEN KING'
-  ACE = 1
-  TWO = 2
-  THREE = 3
-  FOUR = 4
-  FIVE = 5
-  SIX = 6
-  SEVEN = 7
-  EIGHT = 8
-  NINE = 9
-  TEN = 10
-  JACK = 10
-  QUEEN = 10
-  KING = 10
- 
+Rank = {
+  'Ace': 1,
+  'Two': 2,
+  'Three': 3,
+  'Four':  4,
+  'Five': 5,
+  'Six': 6,
+  'Sevem': 7,
+  'Eight': 8,
+  'Nine': 9,
+  'Ten': 10,
+  'Jack': 10,
+  'Queen': 10,
+  'King': 10,
+}
+
 class Card(object):  
   def __init__ (self, rank, suit):
     self.rank = rank
     self.suit = suit
+    
+  def __str__(self):
+    return self.rank + ' of ' + self.suit 
 
 class Deck(object):
   cards = []
@@ -39,7 +41,7 @@ class Deck(object):
     return self.cards.pop(0)       
 
 class Player(object):
-  cards=[]
+  hand=[]
   dealer=False
   name=''
   
@@ -48,10 +50,10 @@ class Player(object):
     self.dealer = dealer
    
   def add_to_hand (self, card):
-    self.cards.append(card)
+    self.hand.append(card)
   
   def get_current_hand(self):
-    return self.cards
+    return self.hand
     
     
 deck = Deck()
@@ -60,6 +62,9 @@ deck.shuffle()
 p1=Player('ME')
 dealer=Player('Dealer', True)
 p1.add_to_hand(deck.draw_card())
+dealer.add_to_hand(deck.draw_card())
+p1.add_to_hand(deck.draw_card())
+dealer.add_to_hand(deck.draw_card())
 
-print(p1.get_current_hand()[0].rank)
+print(p1.get_current_hand()[0])
 
