5f4746eb0b9f938afe96aaa4772405da9ffbdaf7
Mark Penny <mark.penny.jm@gmail.com>
2017-03-25 23:23:57 -0400

    Add dealer's facedown card; determine who won

--- python/21.py
+++ python/21.py
@@ -45,9 +45,14 @@ class Player(object):
     self.hand=[]
     self.name = name
     self.dealer = dealer
+    self.hidden_card = None
+    
    
   def add_to_hand (self, card):
-    self.hand.append(card)
+    if (self.dealer and (self.hidden_card == None)):
+      self.hidden_card = card
+    else:
+      self.hand.append(card)
   
   def get_current_hand(self):
     return self.hand
@@ -56,13 +61,18 @@ class Player(object):
     total = 0
     for card in self.hand:
       total += Rank[card.rank]
+    if self.dealer:
+      total += Rank[self.hidden_card.rank]
     return total
 
 def dealer_turn(dealer, deck):
   while (dealer.hand_value() < 16):
     card = deck.draw_card()
-    player.add_to_hand(card)
-    print('Player ' + player.name + ' was dealt a ' + str(card))
+    dealer.add_to_hand(card)
+    print('Player ' + dealer.name + ' was dealt a ' + str(card))
+    if (dealer.hand_value()> 21):
+      print('Dealer busts!')
+      return
 
 # Instantiate the deck:
 deck = Deck()
@@ -71,19 +81,24 @@ deck.shuffle()
 # Create the players:
 players = []
 players.append(Player('ME'))
-players.append(Player('Dealer', True))
+dealer = Player('Dealer', True)
 
-# Deal initial hands:
+# Deal initial hands to players:
 for player in players:
   for itt in range(0,2):
     player.add_to_hand(deck.draw_card())
- 
+
+for itt in range(0,2):
+  dealer.add_to_hand(deck.draw_card())
+
+print('Dealer\'s hand:')
+print('[Face down card]')
+for card in dealer.get_current_hand():
+  print(str(card))
+   
 for player in players:
-  stay = False
-  if (player.dealer):
-    dealer_turn(player, deck)
-    stay = True
-    
+  stay = False    
+  
   while not stay:
     print(player.name + ' hand:')
     for card in player.get_current_hand():
@@ -103,6 +118,24 @@ for player in players:
     else:
       print('Unknown action')
 
+dealer_turn(dealer, deck)
+
+# Who won?
+print('Dealer\'s hand:')
+print(str(dealer.hidden_card))
+for card in dealer.get_current_hand():
+  print(str(card))
+dealers_total = dealer.hand_value()
+print('Total: ' + str(dealers_total))
+
+for player in players:
+  player_total = player.hand_value()
+  if player_total > dealers_total:
+    print('Player ' + player.name + ': Winner')
+  elif player_total == dealers_total:
+    print('Player ' + player.name + ': Push')
+  elif player_total < dealers_total:
+    print('Player ' + player.name + ': Loser')
     
 
 
