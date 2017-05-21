0b2fb2a0828ca920a6d833aa38a85c85266ab21a
Mark Penny <mark.penny.jm@gmail.com>
2017-05-21 00:26:48 -0400

    Save the image as the provided name

--- nasa_iotd_backgroup.sh
+++ nasa_iotd_backgroup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-IMG_LOCATION="/home/$USER/Pictures/nasa_iotd.jpg"
-curl -X GET `python ./python/nasa.py`-s -o $IMG_LOCATION
+IMG_URL=`python ./python/nasa.py`
+IMG_NAME=`echo $IMG_URL | grep -Eo '[a-zA-Z0-9_-]+\.jpg'`
+IMG_LOCATION="/home/$USER/Pictures/$IMG_NAME"
+curl -X GET $IMG_URL -s -o $IMG_LOCATION
 gsettings set org.gnome.desktop.background picture-uri file:///$IMG_LOCATION
 
 
