cf4ab55e15b2f7ce736a668493705bc11bfb4099
Mark Penny <mark.penny.jm@gmail.com>
2017-10-21 23:22:59 -0400

    Support png as well

--- nasa_iotd_backgroup.sh
+++ nasa_iotd_backgroup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 IMG_URL=`python ./python/nasa.py`
-IMG_NAME=`echo $IMG_URL | grep -Eo '[a-zA-Z0-9_-]+\.jpg'`
+IMG_NAME=`echo $IMG_URL | grep -Eo '[a-zA-Z0-9_-]+\.(png|jpg)'`
 IMG_LOCATION="/home/$USER/Pictures/$IMG_NAME"
 curl -X GET $IMG_URL -s -o $IMG_LOCATION
 gsettings set org.gnome.desktop.background picture-uri file:///$IMG_LOCATION
